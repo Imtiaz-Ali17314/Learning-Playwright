f9df102e0b780b062d7277787ae1ed4063404df1
Imtiaz Ali <191659116+Imtiaz-Ali17314@users.noreply.github.com>
2025-11-28 09:44:30 +0500

    Add login tests with data-driven approach and how to use data from json in playwright.

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,33 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-test.use({
-  viewport: { width: 1200, height: 500 },
-});
+const loginData = JSON.parse(JSON.stringify(require("../login.json")));
 
 test("Login test", async ({ page }) => {
   test.setTimeout(60000);
 
   await page.goto("https://dashboard.softleed.com/auth/login");
 
-  await page
-    .getByPlaceholder("Email")
-    .type("imtiazali17314@gmail.com", { delay: 300 });
+  await page.getByPlaceholder("Email").fill(loginData.email, { delay: 300 });
 
-  await page.getByLabel("Password").type("123", { delay: 200 });
+  await page.getByLabel("Password").fill(loginData.password, { delay: 200 });
 
   await page.getByRole("button", { name: "Login" }).click();
 
-  await page.waitForTimeout(5000);
-
   await expect(page).toHaveURL("https://dashboard.softleed.com/");
+  await page.waitForLoadState("networkidle");
 
-  await page.waitForTimeout(3000);
-
-  await page.getByAltText("profile").click();
+  const profile = page.getByAltText("profile");
+  await expect(profile).toBeVisible();
+  await profile.click();
 
   await page.locator(':text("Log Out")').click();
 
   await expect(page).toHaveURL(/auth/);
-
-  await page.waitForTimeout(3000);
 });
